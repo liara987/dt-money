6f661e4e7ef0166813fad222493cbb03b58c226c
liara987 <987.liara@gmail.com>
2022-06-30 23:08:03 -0300

    chore: Configure reactRefreshWebpackPlugin

--- 01-github-explore/webpack.config.js
+++ 01-github-explore/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const isDevelopment = process.env.NODE_ENV != 'production'
+
+const reactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 module.exports = {
   mode: isDevelopment ? "development" : "production",
   devtool: isDevelopment ? "eval-source-map" : "source-map",
@@ -16,18 +19,27 @@ module.exports = {
     static: path.join(__dirname, "public"),
     compress: true,
     port: 9000,
+    hot: true
   },
   plugins: [
+    isDevelopment && new reactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
     }),
-  ],
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        },
       },
       {
         test: /\.scss$/,
