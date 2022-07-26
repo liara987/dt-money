f4c47726a7d996a774bbbf97019bf8de2dedde36
liara987 <987.liara@gmail.com>
2022-07-26 18:22:45 -0300

    feature: Create Summary component

---
+++ dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+import { Container } from "./styles";
+
+export function Summary(){
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Incomes</p>
+                    <img src={incomeImg} alt="Incomes" />
+                </header>
+                <strong>R$1,000</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Outcome</p>
+                    <img src={outcomeImg} alt="Outcome" />
+                </header>
+                <strong>- R$300,00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$700,00</strong>
+            </div>
+        </Container>
+    )
+}
