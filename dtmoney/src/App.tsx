2fd4de1371c262496a65dca855540cdbb69afa03
liara987 <987.liara@gmail.com>
2022-07-26 18:21:50 -0300

    feature: Basic configuration

---
+++ dtmoney/src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "./styles/global";
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+
+export function App() {
+  return (
+    <>
+      <Header/>
+      <Dashboard/>
+      <GlobalStyle />
+    </>
+  );
+}
