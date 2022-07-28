4d72ea9c4f8d6bf70773a1fc35379833967eaa80
liara987 <987.liara@gmail.com>
2022-07-28 19:51:15 -0300

    feature: Setting up mirage js and adding axios

--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
+import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get('/transaction', () => {
+      return [
+        {
+          id: 1,
+          title: 'First transaction',
+          amount: 300,
+          type: 'Clothes',
+          category: 'Shooping',
+          createdAt: new Date()
+        }
+      ]
+    })
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
