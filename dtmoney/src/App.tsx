289c60b7945723818858bcb51acdd35afcb822da
liara987 <987.liara@gmail.com>
2022-07-28 20:05:59 -0300

    fix: Moving modal to app and adding props to header

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,33 @@
 import { GlobalStyle } from "./styles/global";
-import { Header } from './components/Header';
-import { Dashboard } from './components/Dashboard';
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { useState } from "react";
 
+Modal.setAppElement("#root");
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header/>
-      <Dashboard/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen} />
+      <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalOpen}
+      >
+        <h2>Testing Modal</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,31 +1,15 @@
 import logoImg from "./../../assets/logo.svg";
 import { Container, Content } from "./styles";
-import { useState } from "react";
-import Modal from "react-modal";
 
-export function Header() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-
-  function handleOpenNewTransactionModalOpen() {
-    setIsNewTransactionModalOpen(true);
-  }
-
-  function handleCloseNewTransactionModalOpen() {
-    setIsNewTransactionModalOpen(false);
-  }
-
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+export function Header( {onOpenNewTransactionModal}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Logo dt Money" />
-        <button type="button" onClick={handleOpenNewTransactionModalOpen}>New transaction</button>
-
-        <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModalOpen}
-        >
-          <h2>Testing Modal</h2>
-        </Modal>
+        <button type="button" onClick={onOpenNewTransactionModal}>New transaction</button>
       </Content>
     </Container>
   );
