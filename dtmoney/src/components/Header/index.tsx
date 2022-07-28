bd2cfb2930733b4b8c17bad484067cb0f5aad6e5
liara987 <987.liara@gmail.com>
2022-07-28 19:51:54 -0300

    feature: Adding react modal

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,13 +1,32 @@
-import logoImg from './../../assets/logo.svg'
-import { Container, Content } from './styles';
-export function Header(){
-    return (
-        <Container>
-            <Content>
+import logoImg from "./../../assets/logo.svg";
+import { Container, Content } from "./styles";
+import { useState } from "react";
+import Modal from "react-modal";
 
-            <img src={logoImg} alt="Logo dt Money" />
-            <button type='button'>New transaction</button>
-            </Content>
-        </Container>
-    )
+export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Logo dt Money" />
+        <button type="button" onClick={handleOpenNewTransactionModalOpen}>New transaction</button>
+
+        <Modal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModalOpen}
+        >
+          <h2>Testing Modal</h2>
+        </Modal>
+      </Content>
+    </Container>
+  );
 }
