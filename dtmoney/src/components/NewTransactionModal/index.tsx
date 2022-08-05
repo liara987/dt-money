25bec973e08ec03a7948e5520d32540fe42df769
liara987 <987.liara@gmail.com>
2022-08-05 01:08:06 -0300

    feature: Complete modal transaction

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,16 +1,36 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
-import closeImage from '../../assets/close.svg'
+import incomeImage from "../../assets/income.svg";
+import outcomeImage from "../../assets/outcome.svg";
+import { Container, TransactionTypeContainer, RadioType } from "./styles";
+import closeImage from "../../assets/close.svg";
+import { FormEvent, useState } from 'react';
+import { api } from './../../services/api';
 
 Modal.setAppElement("#root");
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
-export function NewTransactionModal({
-  isOpen,
-  onRequestClose,
-}: NewTransactionModalProps) {
+
+export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [value, setValue] = useState(0)
+  const [type, setType] = useState('deposit')
+
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault()
+    const data = {
+      title,
+      category,
+      value,
+      type
+    };
+    
+    api.post('/transactions', data)
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -18,14 +38,57 @@ export function NewTransactionModal({
       overlayClassName={"react-modal-overlay"}
       className="react-modal-content"
     >
-      <button type="button" onClick={onRequestClose} className="react-modal-close">
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
         <img src={closeImage} alt="X to close" />
       </button>
-      <Container>
+
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Fill transaction</h2>
-        <input type="text" placeholder="Title" />
-        <input type="number" placeholder="Value" />
-        <input type="text" placeholder="Category" />
+        <input
+          type="text"
+          placeholder="Title" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Value" 
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
+
+        <TransactionTypeContainer>
+          <RadioType
+            isActive={type === 'deposit'}
+            type="button"
+            onClick={() => setType('deposit')}
+            backgroundColor="green"
+          >
+            <img src={incomeImage} alt="Income" />
+            <span>Income</span>
+          </RadioType>
+          
+          <RadioType
+            isActive={type === 'withdraw'}
+            type="button"
+            onClick={() => setType('withdraw')}
+            backgroundColor="red"
+          >
+            <img src={outcomeImage} alt="Outcome" />
+            <span>Outcome</span>
+          </RadioType>
+        </TransactionTypeContainer>
+
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </Container>
     </Modal>
